d4b7f892c69ea33683d8c966f6c8b3b40e1ca34b
Pradeep Kumar <118276333+learn2codblog@users.noreply.github.com>
2024-05-26 23:43:14 +0530

    Solution for Caeser cipher problem through java
    ulius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
    
    Original alphabet:      abcdefghijklmnopqrstuvwxyz
    Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
    Example
    
    
    The alphabet is rotated by , matching the mapping above. The encrypted string is .
    
    Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
    
    Function Description
    
    Complete the caesarCipher function in the editor below.
    
    caesarCipher has the following parameter(s):
    
    string s: cleartext
    int k: the alphabet rotation factor
    Returns
    
    string: the encrypted string
    Input Format
    
    The first line contains the integer, , the length of the unencrypted string.
    The second line contains the unencrypted string, .
    The third line contains , the number of letters to rotate the alphabet by.

---
+++ CaeserEncryption.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class CaeserEncryption {
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static String caesarCipher(String s, int k) {
+    // Write your code here
+    
+    StringBuilder result=new StringBuilder();
+    for(char character: s.toCharArray()){
+        if(Character.isLetter(character)){
+            char base=Character.isUpperCase(character)?'A':'a';
+            char shifted=(char)((character-base+k)%26+base);
+            result.append(shifted);
+                }
+                else {
+                // If it's not a letter, append the character unchanged
+                result.append(character);
+                }
+    }
+                return result.toString();
+    
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = CaeserEncryption.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
